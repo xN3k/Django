6373dba91d537a83ba18396c7e6d3405b6c1b3e0
Aadarsh Chaudhary <aadarshchaudhary463@gmail.com>
2021-11-24 20:03:56 +0545

    still getting errors

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html/')
+    return render(request, 'accounts/dashboard.html')
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'accounts/about.html')
 
 def products(request):
-    return HttpResponse('Product List')
+    return render(request, 'accounts/product.html')
 
